0925d167abef7108c4d12efed2f322d32a14790a
Taranvir Hidkyar <taranvirhidkyar@Taranvirs-MacBook-Pro.local>
2018-05-16 09:51:30 -0400

    adding files until url shortening part 2

---
+++ express_server.js
@@ -0,0 +1,65 @@
+var express = require("express");
+var app = express();
+var PORT = process.env.PORT || 8080; // default port 8080
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
+// set the view engine to ejs
+app.set("view engine", "ejs")
+
+var urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+app.get("/", (req, res) => {
+  res.end("Hello!");
+});
+
+app.get("/urls", (req, res) => {
+  let templateVars = {
+    urls: urlDatabase
+  };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.get("/urls/:id", (req, res) => {
+  let templateVars = {
+    shortURL: req.params.id,
+    urls: urlDatabase
+  };
+  res.render("urls_show", templateVars);
+});
+
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
+app.post("/urls", (req, res) => {
+  var shortURL = generateRandomString();
+    urlDatabase[shortURL] = req.body.longURL
+    res.redirect(`urls/${shortURL}`)
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  let shortURL = req.params.shortURL
+  let longURL = urlDatabase[shortURL]
+  if (longURL === undefined) {
+    res.sendStatus(404)
+  }
+    res.redirect(longURL);
+    res.statusCode(301)
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
+
+function generateRandomString() {
+return Math.random().toString(36).substr(2, 6)
+}
---
+++ test.js
@@ -0,0 +1,6 @@
+function generateRandomString() {
+return Math.random().toString(36).substr(2, 6)
+}
+
+
+console.log(generateRandomString())
