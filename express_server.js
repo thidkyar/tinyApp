7b85f7a77611e86fc6a6dcdb0e89e582cdbc7f1e
Taranvir Hidkyar <taranvirhidkyar@Taranvirs-MacBook-Pro.local>
2018-05-17 21:36:26 -0400

    added user specific features

--- express_server.js
+++ express_server.js
@@ -13,8 +13,14 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
 var urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  b2xVn2: {
+    url: "http://www.lighthouselabs.ca",
+    userID: "userRandomID"
+  },
+  "9sm5xK": {
+    url: "http://www.google.com",
+    userID: "userRandomID"
+  }
 };
 
 var users = {
@@ -30,16 +36,36 @@ var users = {
   }
 };
 
+function urlObject(value) {
+  newUrlDatabase = {};
+  for (var id in urlDatabase) {
+    if (urlDatabase[id].userID === value) {
+      newUrlDatabase[id] = urlDatabase[id];
+    }
+  }
+  return newUrlDatabase;
+}
+
 app.get("/", (req, res) => {
   res.end("Hello!");
 });
 
 app.get("/urls", (req, res) => {
+  if (!users[req.cookies.user_id]) {
+    res.redirect("/login");
+  }
+  let uniqUrls = urlObject(req.cookies.user_id);
   let templateVars = {
     users: users[req.cookies["user_id"]],
-    urls: urlDatabase
+    urls: uniqUrls
   };
-  res.render("urls_index", templateVars);
+  if (Object.keys(uniqUrls).length > 0) {
+    res.render("urls_index", templateVars);
+  } else {
+    res.redirect("/urls/new");
+  }
+  // });
+  //   res.render("urls_index", templateVars);
 });
 
 // app.get('/urls/:id')
@@ -48,7 +74,16 @@ app.get("/urls/new", (req, res) => {
   let templateVars = {
     users: users[req.cookies["user_id"]]
   };
-  res.render("urls_new", templateVars);
+  // console.log('users_id', req.cookies['user_id']);
+  for (let id in users) {
+    // console.log('id:',id)
+    // console.log('user[id]: ', users[id])
+    if (users[id].id === req.cookies["user_id"]) {
+      res.render("urls_new", templateVars);
+      return;
+    }
+  }
+  res.redirect("/login");
 });
 
 app.get("/urls/:id", (req, res) => {
@@ -66,18 +101,22 @@ app.get("/urls.json", (req, res) => {
 
 //Posting new URL
 app.post("/urls", (req, res) => {
+  var longURL = req.body.longURL;
   if (!req.body.longURL) {
     res.sendStatus(404);
   } else {
     var shortURL = generateRandomString();
-    urlDatabase[shortURL] = req.body.longURL;
+    urlDatabase[shortURL] = {
+      url: longURL,
+      userID: req.cookies["user_id"]
+    };
+    res.redirect(`/urls/${shortURL}`);
   }
-  res.redirect(`/urls/${shortURL}`);
 });
 
 app.get("/u/:shortURL", (req, res) => {
   let shortURL = req.params.shortURL;
-  let longURL = urlDatabase[shortURL];
+  let longURL = urlDatabase[shortURL].url;
   if (longURL === undefined) {
     res.sendStatus(404);
   }
@@ -87,7 +126,7 @@ app.get("/u/:shortURL", (req, res) => {
 //edit - get
 app.get("/urls/:shortURL", (req, res) => {
   let shortURL = req.params.shortURL;
-  let urls = urlDatabase[shortURL];
+  let urls = urlDatabase[shortURL].url;
 
   if (urls) {
     res.render("urls_show", {
@@ -103,9 +142,9 @@ app.get("/urls/:shortURL", (req, res) => {
 //Update - POST
 app.post("/urls/:id", (req, res) => {
   let shortURL = req.params.id;
-  let urls = urlDatabase[shortURL];
+  let urls = urlDatabase[shortURL].url;
   if (urls) {
-    urlDatabase[shortURL] = req.body.longURL;
+    urlDatabase[shortURL].url = req.body.longURL;
     res.redirect(`/urls/`);
   } else {
     res.redirect("/urls");
@@ -115,10 +154,10 @@ app.post("/urls/:id", (req, res) => {
 //delete Post
 app.post("/urls/:shortURL/delete", (req, res) => {
   let shortURL = req.params.id;
-  let urls = urlDatabase[shortURL];
+  let urls = urlDatabase[shortURL].url;
 
   if (urls) {
-    delete urlDatabase[shortURL];
+    delete urlDatabase[shortURL].url;
   }
   res.redirect("/urls");
 });
@@ -128,26 +167,26 @@ app.get("/login", (req, res) => {
 });
 
 app.post("/login", (req, res) => {
-  const idFromCookie = req.cookies["user_id"];
+  // const idFromCookie = req.cookies["user_id"];
   const loginEmail = req.body.email;
-  const loginPass = req.body.password
-  var userName = users[idFromCookie];
+  const loginPass = req.body.password;
+  // var userName = users[idFromCookie];
   // var err1 = res.send(403, "Sorry, email does not exist");
   // var err2 = res.send(403, "Incorrect email or password");
   if (!loginEmail || !loginPass) {
-    res.send(403, 'Missed an email or password field!')
+    res.send(403, "Missed an email or password field!");
   }
 
   for (let id in users) {
     if (users[id].email === loginEmail) {
-        if (users[id].password === loginPass) {
-      res.cookie("user_id", id);
-      res.redirect("/urls");
-    } else {
-      res.send(403, 'Incorrect email or password!')
+      if (users[id].password === loginPass) {
+        res.cookie("user_id", id);
+        res.redirect("/urls");
+        return;
+      }
     }
   }
-}
+  res.send(403, "Incorrect email or password!");
 });
 
 app.post("/logout", (req, res) => {
